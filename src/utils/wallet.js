7ac754c54362d77af00ed0b7247c743ae6f37ce0
Harry Barber <harrybarber@protonmail.com>
2020-03-21 15:02:56 +0000

    Fix unread aggregate

--- src/utils/wallet.js
+++ src/utils/wallet.js
@@ -1,3 +1,17 @@
 export const calcId = function (output) {
   return output.txId.slice(0, 20) + output.outputIndex
 }
+
+export const stampPrice = function (outpoints) {
+  if (outpoints !== null) {
+    let amount = outpoints.reduce((a, stampOutpoint) => {
+      return stampOutpoint.vouts.reduce((b, vout) => {
+        return stampOutpoint.stampTx.outputs[vout].satoshis + b
+      }, 0) + a
+    }, 0)
+    return amount
+  } else {
+    // We return newline so that the message doesn't move when switching
+    return null
+  }
+}
