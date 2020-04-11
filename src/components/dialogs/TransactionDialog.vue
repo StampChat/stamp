7258b760031bac60eeccae2ae58d705914f270d6
Harry Barber <harrybarber@protonmail.com>
2020-04-11 16:37:52 +0100

    Improve stamp transaction UI

--- src/components/dialogs/TransactionDialog.vue
+++ src/components/dialogs/TransactionDialog.vue
@@ -4,14 +4,45 @@
       <div class="text-h6">{{title}}</div>
     </q-card-section>
     <q-card-section>
-      <q-list>
-        <q-item>
-          <q-item-section>
-            <q-item-label>{{outpoints[0].stampTx.hash}}</q-item-label>
-            <q-item-label caption>Transaction ID</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
+      <q-tabs
+        v-if="outpoints.length !== 1"
+        v-model="outpoint"
+        class="text-primary"
+      >
+        <q-tab v-for="n in outpoints.length"  :key="n" :name="n" :label="n"/>
+      </q-tabs>
+      <q-tab-panel v-for="n in outpoints.length" :key="n" :name="n">
+        <q-item-section class='q-py-lg'>
+          <q-item-label>{{outpoints[n-1].stampTx.hash}}</q-item-label>
+          <q-item-label caption>Transaction ID</q-item-label>
+        </q-item-section>
+        <q-item-section>
+          <span class='text-bold'> Inputs </span>
+          <q-list>
+            <q-item v-for="(input, index) in outpoints[n-1].stampTx.inputs" :key="index">
+              <q-item-section>
+                <span class='text-bold'> Previous Transaction ID </span>
+                {{ input.prevTxId }}
+                <span class='text-bold'> Output Index </span>
+                {{ input.outputIndex }}
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-item-section>
+        <q-item-section>
+          <span class='text-bold'> Outputs </span>
+          <q-list>
+            <q-item v-for="(output, index) in outpoints[n-1].stampTx.outputs" :key="index">
+              <q-item-section>
+                <span class='text-bold'> Address </span>
+                {{ extractAddress(output) }}
+                <span class='text-bold'> Amount </span>
+                {{ output.satoshis }}
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-item-section>
+      </q-tab-panel>
     </q-card-section>
 
     <q-card-actions align="right">
@@ -26,7 +57,23 @@
 </template>
 
 <script>
+const cashlib = require('bitcore-lib-cash')
+
 export default {
-  props: ['title', 'outpoints']
+  props: ['title', 'outpoints'],
+  data () {
+    return {
+      outpoint: 1
+    }
+  },
+  methods: {
+    extractAddress (output) {
+      const script = new cashlib.Script(output.script)
+      if (script.isPublicKeyHashOut()) {
+        return script.toAddress('testnet') // TODO: Make generic
+      }
+      return 'Non-p2pkh'
+    }
+  }
 }
 </script>
