30b3f559a47c6d0dc3d428a1c463477b12d1bde1
Andre Cabrera <andrecabrera@protonmail.ch>
2020-04-13 00:45:21 -0300

    chores - add npm/yarn scripts to generate and clean protobuffers
    Two yarn commands were added: generate:protobuffers and clean:protobuffers. This was intended to facilitate development.
    Now you don't need to have protoc globally installed, as it is installed locally with yarn and then executed locally on the scripts.

--- src/relay/generate_protobufs.sh
+++ src/relay/generate_protobufs.sh
@@ -1,2 +1,2 @@
 echo 'Generating Relay protobuffers...'
-protoc --proto_path=./proto --js_out=import_style=commonjs,binary:. ./proto/*.proto
+../../node_modules/protoc/protoc/bin/protoc --proto_path=./proto --js_out=import_style=commonjs,binary:. ./proto/*.proto
