2726804d114cda70f02328aea2026f156bf0f333
Harry Barber <harrybarber@protonmail.com>
2020-05-10 13:45:44 +0100

    Refactor image utils

--- src/utils/image.js
+++ src/utils/image.js
@@ -1,19 +1,17 @@
-export default {
-  _arrayBufferToBase64 (buffer) {
-    var binary = ''
-    var bytes = new Uint8Array(buffer)
-    var len = bytes.byteLength
-    for (var i = 0; i < len; i++) {
-      binary += String.fromCharCode(bytes[i])
-    }
-    return window.btoa(binary)
-  },
-  entryToImage (entry) {
-    let rawAvatar = entry.getEntryData()
-
-    let value = entry.getHeadersList()[0].getValue()
-    let avatarDataURL = 'data:' + value + ';base64,' + this._arrayBufferToBase64(rawAvatar)
-    return avatarDataURL
+function arrayBufferToBase64 (buffer) {
+  var binary = ''
+  var bytes = new Uint8Array(buffer)
+  var len = bytes.byteLength
+  for (var i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i])
   }
+  return window.btoa(binary)
+}
 
+export const entryToImage = function (entry) {
+  let rawAvatar = entry.getEntryData()
+
+  let value = entry.getHeadersList()[0].getValue()
+  let avatarDataURL = 'data:' + value + ';base64,' + arrayBufferToBase64(rawAvatar)
+  return avatarDataURL
 }
