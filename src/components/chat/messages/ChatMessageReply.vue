10e369afd52bd227c5dd9ab64927437c6f31c6ec
Harry Barber <harrybarber@protonmail.com>
2020-05-19 17:02:27 +0100

    Add names to replies

--- src/components/chat/messages/ChatMessageReply.vue
+++ src/components/chat/messages/ChatMessageReply.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="reply">
+    <div class='col text-weight-bold' :style="`color: ${nameColor};`"> {{ name }} </div>
     <chat-message
       class="row-auto"
       :message="message"
@@ -17,7 +18,10 @@ export default {
     ChatMessage: () => import('./ChatMessage.vue')
   },
   props: {
-    address: String,
+    address: {
+      type: String,
+      required: true
+    },
     payloadDigest: {
       type: String,
       required: true
@@ -25,13 +29,31 @@ export default {
   },
   methods: {
     ...mapGetters({
-      getMessageByPayloadVuex: 'chats/getMessageByPayload'
+      getMessageByPayloadVuex: 'chats/getMessageByPayload',
+      getContact: 'contacts/getContact',
     })
   },
   computed: {
+    ...mapGetters({
+      getProfile: 'myProfile/getProfile'
+    }),
     message () {
       const message = this.getMessageByPayloadVuex()(this.payloadDigest)
       return message || { items: [] }
+    },
+    name () {
+      // let addr = this.message.senderAddress
+      if (this.message.senderAddress === this.$wallet.myAddressStr) {
+        return this.getProfile.name
+      }
+      const contact = this.getContact()(this.message.senderAddress)
+      return contact.profile.name
+    },
+    nameColor () {
+      if (this.message.senderAddress === this.$wallet.myAddressStr) {
+        return 'black'
+      }
+      return this.getContact()(this.address).color
     }
   }
 }
