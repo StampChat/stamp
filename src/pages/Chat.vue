StampChat/stamp

commit 2e8d20fd2d044059774388d23cf5831b5cb19f08

Fix first message duplication (#210)
Prevent first message of a chat from being duplicated.

src/pages/Chat.vue
@@ -243,7 +243,7 @@ export default {
         return []
       }
 
-      const { chunks, currentChunk, globalIndex } = this.messages.reduce(({ chunks, currentChunk, globalIndex }, message) => {
+      const { chunks, currentChunk, globalIndex } = this.messages.slice(1).reduce(({ chunks, currentChunk, globalIndex }, message) => {
         if (currentChunk[0].senderAddress === message.senderAddress) {
           currentChunk.push(message)
           return { chunks, currentChunk, globalIndex: globalIndex + 1 }

commit 0a64fb01e756cf342115ee481442f951537af88c

Fix off by one delete (#212)
Fixes off-by-one bug introduced by #210

src/pages/Chat.vue
@@ -246,14 +246,14 @@ export default {
       const { chunks, currentChunk, globalIndex } = this.messages.slice(1).reduce(({ chunks, currentChunk, globalIndex }, message) => {
         if (currentChunk[0].senderAddress === message.senderAddress) {
           currentChunk.push(message)
-          return { chunks, currentChunk, globalIndex: globalIndex + 1 }
+          return { chunks, currentChunk, globalIndex: globalIndex }
         } else {
-          chunks.push({ chunk: currentChunk, globalIndex: globalIndex - currentChunk.length })
-          return { chunks, currentChunk: [message], globalIndex: globalIndex + 1 }
+          chunks.push({ chunk: currentChunk, globalIndex: globalIndex })
+          return { chunks, currentChunk: [message], globalIndex: globalIndex + currentChunk.length }
         }
       }, { chunks: [], currentChunk: [this.messages[0]], globalIndex: 0 })
 
-      chunks.push({ chunk: currentChunk, globalIndex: globalIndex - currentChunk.length })
+      chunks.push({ chunk: currentChunk, globalIndex: globalIndex })
       return chunks
     },
     nameColor () {