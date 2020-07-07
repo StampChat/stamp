9eaafb603361e1b2d8cea128eadb3cf61dfe4d15
Harry Barber <harrybarber@protonmail.com>
2020-07-07 10:26:43 +0100

    Migrate to new protobuf files

--- src/bip70/generate_protobufs.sh
+++ src/bip70/generate_protobufs.sh
@@ -1,2 +1,2 @@
-echo 'Generating Proof-of-Payment protobuffers...'
+echo 'Generating BIP70 protobuffers...'
 ../../node_modules/protoc/protoc/bin/protoc --proto_path=./proto --js_out=import_style=commonjs,binary:. ./proto/*.proto
