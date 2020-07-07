86fde7f5ac1e127b96c701a5bc845d6423fe0038
Shammah Chancellor <shammah.chancellor@gmail.com>
2020-07-06 18:06:13 -0700

    Fix tab rendering for the transaction dialog and add type info (#231)
    Currently, the rendering of the tabs in this dialog is incorrect. It
    does not have the `q-tab-panel` wrapped with the appropriate control,
    and sharing a model with the `q-tabs` element. This commit fixes
    that issue, as well as adding a type field and renaming the menu item.
    
    The second two changes are because this dialog also shows other types
    of outpoints attached to a message.

--- src/components/dialogs/TransactionDialog.vue
+++ src/components/dialogs/TransactionDialog.vue
@@ -6,21 +6,28 @@
     <q-card-section>
       <q-tabs
         v-if="outpoints.length !== 1"
-        v-model="outpoint"
+        v-model="tab"
         class="text-primary"
       >
         <q-tab v-for="n in outpoints.length"  :key="n" :name="n" :label="n"/>
       </q-tabs>
-      <q-tab-panel v-for="n in outpoints.length" :key="n" :name="n">
-        <q-item-section class='q-py-lg'>
-            <span class='text-bold'> Transaction ID </span>
-            <q-item-label>{{outpoints[n-1].txId}}</q-item-label>
-            <span class='text-bold'> Address </span>
-            {{ outpoints[n-1].address}}
-            <span class='text-bold'> Amount </span>
-            {{ outpoints[n-1].satoshis }}
-        </q-item-section>
-      </q-tab-panel>
+      <q-tab-panels
+        v-model="tab"
+        animated
+      >
+        <q-tab-panel v-for="n in outpoints.length" :key="n" :name="n">
+          <q-item-section class='q-py-lg'>
+              <span class='text-bold'> Transaction ID </span>
+              <q-item-label>{{outpoints[n-1].txId}}</q-item-label>
+              <span class='text-bold'> Type </span>
+              <q-item-label>{{outpoints[n-1].type}}</q-item-label>
+              <span class='text-bold'> Address </span>
+              {{ outpoints[n-1].address}}
+              <span class='text-bold'> Amount </span>
+              {{ outpoints[n-1].satoshis }}
+          </q-item-section>
+        </q-tab-panel>
+      </q-tab-panels>
     </q-card-section>
 
     <q-card-actions align="right">
@@ -41,7 +48,8 @@ export default {
   props: ['title', 'outpoints'],
   data () {
     return {
-      outpoint: 1
+      outpoint: 1,
+      tab: 1
     }
   },
   methods: {
