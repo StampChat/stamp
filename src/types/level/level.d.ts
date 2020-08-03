7e49bdcf70ac17a1a67ddd170c10c1475b7fe238
Shammah Chancellor <shammah.chancellor@gmail.com>
2020-08-03 09:50:15 -0700

    Improve `level` module TypeScript definitions
    Based on information provided from from Jason Dreyzehner, we're updating
    the TS declaration for level. This makes things a bit cleaner when
    using the level module in typescript.

--- src/types/level/level.d.ts
+++ src/types/level/level.d.ts
@@ -1,18 +1,18 @@
 declare module 'level' {
-   class LevelDBIterator {
-     next(): Promise<string>
-     end(): Promise<void>
-   }
+  export class LevelDBIterator {
+    next(): Promise<string>
+    end(): Promise<void>
+  }
 
- class LevelDB {
-   put(key: string, value: string): Promise<void>;
-   get(key: string): Promise<string>;
-   del(key: string): Promise<void>;
+  export class LevelDB {
+    put(key: string, value: string): Promise<void>;
+    get(key: string): Promise<string>;
+    del(key: string): Promise<void>;
 
-   close(): Promise<void>;
-   iterator(options: Record<string, unknown>): Promise<LevelDBIterator>
- }
+    close(): Promise<void>;
+    iterator(options: Record<string, unknown>): Promise<LevelDBIterator>
+  }
 
   function level(localtion: string): LevelDB;
-  export = level;
+  export default level
 }
