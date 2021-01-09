0152bbdb885e7572b837979c9e51bc474583687e
Shammah Chancellor <shammah.chancellor@gmail.com>
2021-01-09 13:30:28 -0800

    Fix several type issues with delete message that had occured (#365)
    During past refactors, a few issues with delete message and other
    parts of the codebase were broken. This commit fixes the issues around
    forwarding deleted UTXOs and an accidental inversion of conditions when
    receiving a message with a payload digest instead of a full message.

--- src/relay/extension.js
+++ src/relay/extension.js
@@ -79,23 +79,22 @@ export const messageMixin = {
   digest () {
     const payloadDigest = this.getPayloadDigest()
     const payload = this.getPayload()
+    const payloadBuffer = Buffer.from(payload)
     switch (payloadDigest.length) {
       case 0:
         if (!payload.length) {
           throw new Error('Missing payload and digest')
         }
-        return crypto.Hash.sha256(payload)
+        return crypto.Hash.sha256(payloadBuffer)
       case 32:
-        if (!payload.length) {
-          const computedPayloadDigest = crypto.Hash.sha256(payload)
-
-          if (payloadDigest !== computedPayloadDigest) {
-            throw new Error('Fraudulent payload digest')
+        if (payload.length) {
+          const computedPayloadDigest = crypto.Hash.sha256(payloadBuffer)
+          const computedDigest = Buffer.from(computedPayloadDigest)
+          if (computedDigest.compare(payloadDigest) !== 0) {
+            throw new Error(`Fraudulent payload digest: ${payloadDigest} !== ${computedDigest}`)
           }
-          return payloadDigest
-        } else {
-          return payload
         }
+        return payloadDigest
       default:
         throw new Error('Unexpected length payload digest')
     }
