bf8745ffa4e8b155e8b6a0f5374ff689b9ed2bfd
Shammah Chancellor <shammah.chancellor@gmail.com>
2021-01-11 16:39:16 -0800

    Add EULA to the sign in step due to the lack of HA backend nodes
    Currently, our backend nodes are not HA. In the case that they fail some
    funds could be lost. We need to warn users of this, and make them aware
    that we will not be covering any loss.

---
+++ src/components/setup/EULAStep.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="col q-gutter-y-md">
+    <div class="row p-mx-sm">
+      <q-space />
+      <q-img
+        src="~assets/donation_qr_code.jpg"
+        width="50vw"
+        spinner-color="white"
+      />
+      <q-space />
+    </div>
+
+    <div class="row q-gutter-y-md">
+      This software is very early stage, and may lose funds. Keep larger amounts of eCash in more established wallets.
+    </div>
+
+    <div class="row q-gutter-y-md">
+      By clicking "Agree", you understand that this software is provided "As is", without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and non-infringement.
+    </div>
+    </q-img>
+  </div>
+</template>
+
+<script>
+export default {
+  model: {
+    event: 'input'
+  },
+  props: {
+  },
+  data () {
+    return {}
+  },
+  methods: {},
+  computed: {}
+}
+</script>
