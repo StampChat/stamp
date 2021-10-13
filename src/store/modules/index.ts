6a566bf2085a1b8ed57dddd063d2bc6f5702696e
Shammah Chancellor <shammah.chancellor@gmail.com>
2021-10-12 17:46:50 -0700

    Convert index file of store modules to typescript
    This is both a ts conversion and fixes an issue with importing
    typescript store modules. They were not being picked up due to
    regex filename magic. We don't have so many different stores that
    this magic import setup is particularly worthwhile and it just
    adds complexity -- and caused some things to break.

---
+++ src/store/modules/index.ts
@@ -0,0 +1,17 @@
+import appearance from './appearance'
+import chats from './chats'
+import contacts from './contacts'
+import myProfile from './my_profile'
+import relayClient from './relay_client'
+import wallet from './wallet'
+
+const modules = {
+  appearance,
+  chats,
+  contacts,
+  myProfile,
+  relayClient,
+  wallet
+}
+
+export default modules
