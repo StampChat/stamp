0daebf5474576048980e7496f722a6470958de66
Shammah Chancellor <shammah.chancellor@gmail.com>
2021-10-12 17:48:59 -0700

    Remove unused `client` from relay store
    This part of the `relayClient` store was no longer used as it is
    now being handled explicitly as a Vuex property in the `setup-apis`
    boot file.

--- src/store/modules/relay_client.ts
+++ src/store/modules/relay_client.ts
@@ -1,8 +1,6 @@
-import RelayClient from 'src/cashweb/relay'
 import type { Module } from 'vuex'
 
 type State = {
-  client: RelayClient | null;
   token: string | null;
 };
 
@@ -12,21 +10,14 @@ type RootState = any;
 const module: Module<State, RootState> = {
   namespaced: true,
   state: {
-    client: null,
     token: null
   },
   mutations: {
-    setClient (state, client: RelayClient) {
-      state.client = client
-    },
     setToken (state, token: string) {
       state.token = token
     }
   },
   actions: {
-    setConnected ({ commit }, connected: boolean) {
-      commit('setConnected', connected)
-    },
     setToken ({ commit }, token: string) {
       commit('setToken', token)
     }
