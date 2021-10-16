74d9638b1dc8be7e4ad0063d589d975f42e0a189
Shammah Chancellor <shammah.chancellor@gmail.com>
2021-10-16 13:03:50 -0700

    Convert POP token client to typescript

--- src/cashweb/pop.ts
+++ src/cashweb/pop.ts
@@ -1,10 +1,12 @@
-import axios from 'axios'
-import paymentrequest from './bip70/paymentrequest_pb'
-
+import assert from 'assert'
+import axios, { Method } from 'axios'
 import { Transaction } from 'bitcore-lib-xpi'
 
+import paymentrequest, { Payment, PaymentDetails } from './bip70/paymentrequest_pb'
+import { Wallet } from './wallet'
+
 export default {
-  async getPaymentRequest (url, method, data) {
+  async getPaymentRequest (url: string, method: Method, data?: Uint8Array) {
     try {
       await axios({
         method,
@@ -12,18 +14,20 @@ export default {
         responseType: 'arraybuffer',
         data
       })
-    } catch (err) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
       const response = err.response
       if (response.status === 402) {
         const paymentRequest = paymentrequest.PaymentRequest.deserializeBinary(response.data)
         const serializedPaymentDetails = paymentRequest.getSerializedPaymentDetails()
+        assert(typeof serializedPaymentDetails !== 'string', 'serializedPaymentDetails of wrong type?')
         const paymentDetails = paymentrequest.PaymentDetails.deserializeBinary(serializedPaymentDetails)
         return { paymentRequest, paymentDetails }
       }
     }
   },
 
-  async sendPayment (paymentUrl, payment) {
+  async sendPayment (paymentUrl: string, payment: Payment) {
     const rawPayment = payment.serializeBinary()
     const response = await axios({
       method: 'post',
@@ -40,7 +44,7 @@ export default {
     return { paymentReceipt, token }
   },
 
-  async constructPaymentTransaction (wallet, paymentDetails) {
+  async constructPaymentTransaction (wallet: Wallet, paymentDetails: PaymentDetails) {
     // Get Outputs
     const requestOutputs = paymentDetails.getOutputsList()
     const outputs = requestOutputs.map(reqOutput => {
@@ -62,7 +66,7 @@ export default {
     payment.addTransactions(rawTransaction)
     payment.setMerchantData(paymentDetails.getMerchantData())
     const paymentUrl = paymentDetails.getPaymentUrl()
-
+    assert(typeof paymentUrl === 'string', 'Payment url undefined when it should not be')
     return { payment, paymentUrl, usedUtxos }
   }
 }
