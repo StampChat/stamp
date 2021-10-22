d3559cb260cee6a3c12bf754e92f8f24f7251e80
Shammah Chancellor <shammah.chancellor@gmail.com>
2021-10-22 15:37:30 -0700

    Fix generate_protobuf message for BIP70 protos
    As per title.

--- src/cashweb/bip70/generate_protobufs.sh
+++ src/cashweb/bip70/generate_protobufs.sh
@@ -1,4 +1,4 @@
-echo 'Generating Authorization Wrapper protobuffers...'
+echo 'Generating BIP70 protobuffers...'
 ../../../node_modules/protoc/protoc/bin/protoc \
   --proto_path=./proto \
   --js_out=import_style=commonjs,binary:. \
