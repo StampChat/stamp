eacef27b83dc4005686b70bb422195bc793f3c7d
Shammah Chancellor <shammah.chancellor@gmail.com>
2021-10-28 21:46:14 -0700

    Factor out message component from Agora.vue page
    This commit factors out the agora message component from the main vue
    page. This makes it easier to modify independently of the other things
    on the page. But, more importantly it is required for recursive
    elements. Recursing on message will be required for replies to be able
    to function correctly.

--- src/pages/Agora.vue
+++ src/pages/Agora.vue
@@ -50,81 +50,12 @@
         >
           <template
             v-for="(message) in messages"
-            :key="message.payloadHash"
+            :key="message.payloadDigest"
           >
-            <q-card
-              class="q-ma-sm q-pa-none"
-              flat
-              bordered
-            >
-              <q-card-section
-                class="row"
-                horizontal
-              >
-                <q-card-section>
-                  <div class="col-shrink">
-                    {{ formatSatoshis(message.satoshis) }}
-                  </div>
-                </q-card-section>
-                <q-separator vertical />
-                <q-card-section
-                  class="q-ma-none q-pa-none col"
-                  vertical
-                >
-                  <q-card-section
-                    class="q-ma-none q-pa-sm col-grow"
-                    horizontal
-                  >
-                    <q-btn
-                      no-caps
-                      flat
-                      stretch
-                      padding="1px"
-                      :to="`/chat/${message.poster}`"
-                    >
-                      <div
-                        v-if="haveContact(message.poster)"
-                      >
-                        {{ getContactProfile(message.poster).name }}
-                      </div>
-                      <div v-else>
-                        {{ formatAddress(message.poster) }}
-                      </div>
-                    </q-btn>
-                    <q-space />
-                    <q-btn
-                      no-caps
-                      flat
-                      stretch
-                      padding="1px"
-                      @click.prevent="setTopic(message.topic)"
-                      :label="message.topic"
-                    />
-                  </q-card-section>
-
-                  <q-separator horizontal />
-
-                  <template
-                    v-for="(entry, index) in message.entries.filter(entry=> entry.kind === 'post')"
-                    :key="index"
-                  >
-                    <q-card-section class="q-ma-none q-pa-sm col-grow text-bold">
-                      <a
-                        :href="entry.url"
-                        v-if="entry.url"
-                      >{{ entry.title }}</a>
-                      <span v-if="!entry.url">{{ entry.title }}</span>
-                    </q-card-section>
-                    <q-card-section class="q-ma-none q-pa-sm col-grow">
-                      <span
-                        class="mdstyle"
-                        v-html="markedMessage(entry.message)"
-                      />
-                    </q-card-section>
-                  </template>
-                </q-card-section>
-              </q-card-section>
-            </q-card>
+            <a-message
+              :message="message"
+              @set-topic="setTopic"
+            />
           </template>
         </q-scroll-area>
       </q-page>
@@ -136,13 +67,16 @@
 import marked from 'marked'
 import DOMPurify from 'dompurify'
 
-import { AgoraMessage } from 'src/cashweb/types/agora'
+import { AgoraMessage } from '../cashweb/types/agora'
 import { defineComponent } from 'vue'
 import { mapGetters, mapActions, mapMutations } from 'vuex'
+import AMessage from '../components/agora/AgoraMessage.vue'
 
 export default defineComponent({
   props: {},
-  components: {},
+  components: {
+    AMessage
+  },
   mounted () {
     this.refreshContent()
   },
@@ -176,14 +110,15 @@ export default defineComponent({
     },
     formatAddress (address:string) {
       return address.substring(6, 12) + '...' + address.substring(address.length - 6, address.length)
+    },
+    replyToMessage (payloadDigest: string) {
+      console.log(payloadDigest)
     }
   },
   computed: {
     ...mapGetters({
       getMessages: 'agora/getMessages',
       topics: 'agora/getTopics',
-      getContactProfile: 'contacts/getContactProfile',
-      haveContact: 'contacts/haveContact',
       getSelectedTopic: 'agora/getSelectedTopic'
     }),
     messages () {
@@ -206,19 +141,3 @@ export default defineComponent({
   }
 })
 </script>
-
-<style scoped>
-  :deep() .mdstyle img {
-    max-width: 100%;
-    max-height: 448px;
-  }
-  :deep() .mdstyle pre, code, table {
-    /*overflow-wrap: break-word;*/
-    max-width: 100%;
-    white-space: pre-wrap;
-  }
-  :deep() .mdstyle p {
-    max-width: 100%;
-    word-break: break-all;
-  }
-</style>
