d1fc262f17b1296298334487a57ea56b785be42e
Shammah Chancellor <shammah.chancellor@gmail.com>
2021-10-31 19:08:19 -0700

    Add some basic form validation on Agora post creation
    Currently the backend is soly responsible for post validation on Agora.
    This adds some basic feedback to users prior to getting backend errors.

--- src/pages/CreatePost.vue
+++ src/pages/CreatePost.vue
@@ -1,44 +1,59 @@
 <template>
   <q-card class="q-ma-sm">
-    <q-card-section>
-      <q-input
-        label="Offering"
-        v-model="offering"
-        suffix="XPI"
-      />
-      <q-input
-        label="Topic"
-        :disable="!!getMessage(parentDigest)"
-        v-model="topic"
-      />
-      <q-input
-        label="Post Title"
-        v-model="title"
-      />
-      <q-input
-        label="URL"
-        v-model="url"
-      />
-      <q-input
-        label="Message"
-        v-model="message"
-        type="textarea"
-      />
-    </q-card-section>
-    <q-card-actions align="right">
-      <q-btn
-        @click="back"
-        label="back"
-        color="negative"
-        class="q-ma-sm"
-      />
-      <q-btn
-        @click="post"
-        label="Post"
-        color="primary"
-        class="q-ma-sm"
-      />
-    </q-card-actions>
+    <q-form @submit="post">
+      <q-card-section>
+        <q-input
+          label="Offering"
+          v-model="offering"
+          suffix="XPI"
+          :rules="[
+            val => Number.parseFloat(val) || 'Invalid number'
+          ]"
+          lazy-rules
+        />
+        <q-input
+          label="Topic"
+          :disable="!!getMessage(parentDigest)"
+          v-model="topic"
+          :rules="[
+            val => val && val.length >0 && (/^[a-z0-9.-]+$/).test(val) || 'Only numbers, lowercase, periods and dashes allowed',
+            val => !val.split('.').some(val => val.length === 0) || 'Topic segments not allowed to be empty'
+          ]"
+          lazy-rules
+        />
+        <q-input
+          label="Post Title"
+          v-model="title"
+        />
+        <q-input
+          label="URL"
+          v-model="url"
+          :rules="[
+            val => (val.length >= 0 && validateUrl(val)) || 'Invalid URL'
+          ]"
+          lazy-rules
+        />
+        <q-input
+          label="Message"
+          v-model="message"
+          type="textarea"
+        />
+      </q-card-section>
+      <q-card-actions align="right">
+        <q-btn
+          @click="back"
+          label="back"
+          color="negative"
+          class="q-ma-sm"
+        />
+        <q-btn
+          type="submit"
+          label="Post"
+          color="primary"
+          class="q-ma-sm"
+        />
+      </q-card-actions>
+    </q-form>
   </q-card>
 
   <q-card
@@ -69,7 +84,7 @@ export default defineComponent({
   data () {
     const parentDigest = this.$route.params.parentDigest as string
     return {
-      offering: 1,
+      offering: 10,
       topic: (this.$store.state.agora.index)[parentDigest]?.topic ?? '',
       title: '',
       url: null,
@@ -103,6 +118,15 @@ export default defineComponent({
     },
     back () {
       window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
+    },
+    validateUrl (val: string) {
+      try {
+        const url = new URL(val)
+        return !!url
+      } catch (err) {
+        console.log(err)
+        return false
+      }
     }
   }
 })
