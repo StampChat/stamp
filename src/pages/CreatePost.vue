57b813dc5ea1e86dd1ba6a3f69dd669440e1ab07
maff1989 <matthew@urgero.net>
2021-11-11 22:20:28 -0600

    Add message markdown preview

--- src/pages/CreatePost.vue
+++ src/pages/CreatePost.vue
@@ -108,6 +108,7 @@ export default defineComponent({
   components: {
     AMessage
   },
+  emits: ['setTopic'],
   props: {},
   data () {
     const parentDigest = this.$route.params.parentDigest as string
