45550d46f7ec54ff37ab4807b1fd55388f1648cd
maff1989 <maff1989@stamprepo.local>
2021-11-12 21:39:23 -0600

    Remove 'title' attrib from 'a' tag

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -1,7 +1,7 @@
 import marked from 'marked'
 const renderer = new marked.Renderer()
 renderer.link = (href, title, text) => {
-  return '<a target="_blank" href="' + href + '" title="' + title + '">' + text + '</a>'
+  return '<a target="_blank" href="' + href + '">' + text + '</a>'
 }
 
 export { marked, renderer }
