9758b0d505b627bc47a39543c262622daa31a5c2
Shammah Chancellor <shammah.chancellor@gmail.com>
2021-11-13 11:38:22 -0800

    Protect `/forum/new-post` route for non-signed up users (#481)
    During the re-org of the routes, creation of a post became "unprotected"
    but it actually does require an account. This commit ensures that users
    who are not yet signed up are redirected to the sign up page.

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,7 @@ import { createRoutes } from './routes'
 import { RootState } from 'src/store/modules'
 
 const unprotectedRoutes = ['/', '/setup', '/forum', '/changelog']
+const protectedRoutes = ['/forum/new-post']
 
 async function addContactFromNavigation<S> (store: Store<S>, address: string) {
   try {
@@ -39,7 +40,7 @@ export default ({ store }: { store: Store<RootState> }) => {
       addContactFromNavigation(store, to.params.address as string)
     }
     console.log('Navigating to', to.fullPath, to.params.address, profile.name)
-    if (profile.name || unprotectedRoutes.some(path => to.fullPath.startsWith(path))) {
+    if (profile.name || (unprotectedRoutes.some(path => to.fullPath.startsWith(path)) && !protectedRoutes.some(path => to.fullPath.startsWith(path)))) {
       next()
     } else {
       console.log('nav to setup!')
