9e4066e7b32faa25c2f4edf911646ca33b66b9e4
maff1989 <maff1989@stamprepo.local>
2021-11-15 11:20:59 -0600

    add link icon to post titles containing URL

--- src/components/forum/ForumMessage.vue
+++ src/components/forum/ForumMessage.vue
@@ -44,12 +44,20 @@
             horizontal
             class="q-ma-none q-pa-sm col-grow text-bold"
           >
+            <q-icon
+              name="link"
+              v-if="entry.url"
+              size="24px"
+              class="q-pa-xs"
+            />
             <a
               :href="entry.url"
               target="_blank"
               v-if="entry.url"
               class="text-h6 text-bold q-mr-md post-title"
-            >{{ entry.title || 'Untitled' }}</a>
+            >
+              {{ entry.title || 'Untitled' }}
+            </a>
             <span
               v-if="!entry.url"
               class="text-h6 text-bold q-mr-md"
