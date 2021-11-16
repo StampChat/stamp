770b2da0095efb3a8057d2512535ab4d781db8a0
Vic Cieślak <wiktorcie@gmail.com>
2021-11-16 15:50:52 +0100

    Add spinner at forum page before posts are loaded (#499)

--- src/pages/Forum.vue
+++ src/pages/Forum.vue
@@ -1,16 +1,27 @@
 <template>
-  <template
-    v-for="(message) in sortedPosts"
-    :key="message.payloadDigest"
-  >
-    <a-message
-      v-show="showMessage(message.topic)"
-      @set-topic="(...args) => $emit('set-topic', ...args)"
-      :message="message"
-      :show-parent="true"
-      :show-replies="false"
-      :compact-view="compactView"
-    />
+  <template v-if="messages.length > 0">
+    <template
+      v-for="(message) in sortedPosts"
+      :key="message.payloadDigest"
+    >
+      <a-message
+        v-show="showMessage(message.topic)"
+        @set-topic="(...args) => $emit('set-topic', ...args)"
+        :message="message"
+        :show-parent="true"
+        :show-replies="false"
+        :compact-view="compactView"
+      />
+    </template>
+  </template>
+  <template v-else>
+    <div>
+      <q-spinner-puff
+        class="absolute-center"
+        color="purple"
+        size="20rem"
+      />
+    </div>
   </template>
 </template>
 
