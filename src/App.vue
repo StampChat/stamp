45e5c1b1da2ccc4b92a0cce29631393a2fb8ec23
Shammah Chancellor <shammah.chancellor@gmail.com>
2022-09-02 14:22:33 -0700

    Add a prototype of automatically adding localized topics
    Automatically add topics based on IP localization, so that people can
    hopefully find individuals close to them to meet up for coffee, or buy
    and sell other goods.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,47 @@
 <template>
   <router-view />
 </template>
+
+<script lang="ts">
+import assert from 'assert'
+import axios from 'axios'
+import { defineComponent } from 'vue'
+import { useTopicStore } from './stores/topics'
+
+export default defineComponent({
+  setup() {
+    /**
+     * Setup default localized topics
+     */
+    axios({
+      method: 'get',
+      url: 'https://json.geoiplookup.io/',
+      responseType: 'json',
+    }).then(response => {
+      if (response.status !== 200) {
+        return
+      }
+      const topicStore = useTopicStore()
+      const { country_code, region, district } = response.data
+      const topicParts = [country_code, region, district] as string[]
+      const topics = topicParts.reduce(
+        (topics, topicPart) => {
+          const lastTopic = topics.pop()
+          assert(lastTopic !== undefined, 'Should be world')
+          const normalizedTopicPart = topicPart
+            .trim()
+            .toLowerCase()
+            .replace(/\s/, '-')
+          const joinedTopic = [lastTopic, normalizedTopicPart].join('.')
+          return [...topics, lastTopic, joinedTopic]
+        },
+        ['world'],
+      )
+      for (const topic of topics) {
+        topicStore.ensureTopic(topic)
+      }
+      console.log('topics', topics)
+    })
+  },
+})
+</script>
