73895addf77f7a735bd5b6be4a36c94200a7ef47
Shammah Chancellor <shammah.chancellor@gmail.com>
2023-03-11 08:50:18 -0800

    [topics] Remove setting up default topics based on IP address
    This is obnoxious and results in me getting all kinds of topics on
    the left hand side. Delete them for now, maybe we'll add something
    better in later.

--- src/App.vue
+++ src/App.vue
@@ -39,39 +39,6 @@ export default defineComponent({
     ContactBookDialog,
   },
   setup() {
-    /**
-     * Setup default localized topics
-     */
-    axios({
-      method: 'get',
-      url: 'https://json.geoiplookup.io/',
-      responseType: 'json',
-    }).then(response => {
-      if (response.status !== 200) {
-        return
-      }
-      const topicStore = useTopicStore()
-      const { country_code, region, district } = response.data
-      const topicParts = [country_code, region, district] as string[]
-      const topics = topicParts.reduce(
-        (topics, topicPart) => {
-          const lastTopic = topics.pop()
-          assert(lastTopic !== undefined, 'Should be world')
-          const normalizedTopicPart = topicPart
-            .trim()
-            .toLowerCase()
-            .replace(/\s/g, '-')
-          const joinedTopic = [lastTopic, normalizedTopicPart].join('.')
-          return [...topics, lastTopic, joinedTopic]
-        },
-        ['world'],
-      )
-      for (const topic of topics) {
-        topicStore.ensureTopic(topic)
-      }
-      console.log('topics', topics)
-    })
-
     // Setup chats, contacts, etc.
     const chatStore = useChatStore()
     const relayClient = useRelayClientStore()
